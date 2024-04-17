e45eff70051fad4c6ed3d07ba15442f1bae07e0a
GildoParuque <gildoernestoparuque82@gmail.com>
2024-04-17 11:06:18 +0200

    Muita coisa nova adicionada

--- Restaurants.Domain/Entities/Restaurant.cs
+++ Restaurants.Domain/Entities/Restaurant.cs
@@ -10,7 +10,7 @@
         public string? ContactEmail { get; set; }
         public string? ContactNumber { get; set;}
         public Address? Address { get; set;}
-        public List<Dish> Dishes { get; set; } new();
+        public List<Dish> Dishes { get; set; } = new();
 
     }
 }
---
+++ Restaurants.Infrastruture/Persistence/RestaurantsDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Restaurants.Domain.Entities;
+
+namespace Restaurants.Infrastruture.Persistence;
+
+internal class RestaurantsDbContext(DbContextOptions<RestaurantsDbContext> options) : DbContext(options)
+{
+    internal DbSet<Restaurant> Restaurants { get; set;}
+    internal DbSet<Dish> Dishes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Restaurant>()
+           .OwnsOne(c => c.Address);
+
+        modelBuilder.Entity<Restaurant>()
+            .HasMany(c => c.Dishes)
+            .WithOne()
+            .HasForeignKey(c => c.RestaurantId);
+    }
+}
+
